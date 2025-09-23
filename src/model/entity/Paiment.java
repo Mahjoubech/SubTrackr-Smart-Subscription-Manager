fbd5f72b5c88af9a166a1adc39d29b2bef99043f
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-23 04:32:24 +0100

    Add constructors, getters/setters, and toString() : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-21

--- src/model/entity/Paiment.java
+++ src/model/entity/Paiment.java
@@ -19,4 +19,33 @@ public class Paiment {
         this.typePaiement = typePaiement;
         this.statut = statut;
     }
+
+    public String getIdPaiement() { return idPaiement; }
+    public String getIdAbonnement() { return idAbonnement; }
+    public void setIdAbonnement(String idAbonnement) { this.idAbonnement = idAbonnement; }
+
+    public LocalDate getDateEcheance() { return dateEcheance; }
+    public void setDateEcheance(LocalDate dateEcheance) { this.dateEcheance = dateEcheance; }
+
+    public LocalDate getDatePaiement() { return datePaiement; }
+    public void setDatePaiement(LocalDate datePaiement) { this.datePaiement = datePaiement; }
+
+    public String getTypePaiement() { return typePaiement; }
+    public void setTypePaiement(String typePaiement) { this.typePaiement = typePaiement; }
+
+    public StatusPaiement getStatut() { return statut; }
+    public void setStatut(StatusPaiement statut) { this.statut = statut; }
+
+
+    @Override
+    public String toString() {
+        return "Paiement{" +
+                "idPaiement='" + idPaiement + '\'' +
+                ", idAbonnement='" + idAbonnement + '\'' +
+                ", dateEcheance=" + dateEcheance +
+                ", datePaiement=" + datePaiement +
+                ", typePaiement='" + typePaiement + '\'' +
+                ", statut=" + statut +
+                '}';
+    }
 }
