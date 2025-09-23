369a8d91b3e43f9f65223030e6cd84af9a8f6274
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-23 04:23:40 +0100

    create AbonnementSansEngagement extends Abonnement: No extra fields. : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-17

--- src/model/entity/AbonnementSansEngagement.java
+++ src/model/entity/AbonnementSansEngagement.java
@@ -1,4 +1,10 @@
 package model.entity;
+import model.enums.StatusAbonnement;
+import java.time.LocalDate;
 
-public class AbonnementSansEngagement {
+public class AbonnementSansEngagement extends Abonnement{
+
+    public AbonnementSansEngagement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatusAbonnement statut) {
+        super(nomService, montantMensuel, dateDebut, dateFin, statut);
+    }
 }
