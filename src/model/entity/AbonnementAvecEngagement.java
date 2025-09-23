cd49f33e769e59125a2b74a5c2bbbbebef7e12cf
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-23 04:21:36 +0100

    create AbonnementAvecEngagement extends Abonnement : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-17

--- src/model/entity/AbonnementAvecEngagement.java
+++ src/model/entity/AbonnementAvecEngagement.java
@@ -1,4 +1,13 @@
 package model.entity;
+import model.enums.StatusAbonnement;
+import java.time.LocalDate;
 
-public class AbonnementAvecEngagement {
+public class AbonnementAvecEngagement extends Abonnement{
+    private int dureeEngagementMois;
+
+    public AbonnementAvecEngagement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatusAbonnement statut, int dureeEngagementMois) {
+        super(nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = dureeEngagementMois;
+    }
+
 }
