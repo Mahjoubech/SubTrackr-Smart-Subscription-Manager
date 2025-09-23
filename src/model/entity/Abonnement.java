d21f8e280e817c44aba38bb3c4e46bb1562e1cfc
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-23 04:14:27 +0100

    Define Abonnement as abstract class : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-14

--- src/model/entity/Abonnement.java
+++ src/model/entity/Abonnement.java
@@ -1,7 +1,39 @@
 package model.entity;
+import jdk.net.SocketFlow;
+import model.enums.StatusAbonnement;
+import java.time.LocalDate;
+import java.util.UUID;
 
 public abstract class Abonnement {
-    protected String id ;
+    protected String id;
+    protected String nomService;
+    protected double montantMensuel;
+    protected LocalDate dateDebut;
+    protected LocalDate dateFin;
+    protected StatusAbonnement statut;
+    public Abonnement(String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, StatusAbonnement statut) {
+          this.id = UUID.randomUUID().toString();
+          this.nomService = nomService;
+          this.montantMensuel = montantMensuel;
+          this.dateDebut = dateDebut;
+          this.dateFin = dateFin;
+          this.statut = statut;
+    }
+    public String getId() { return id; }
+    public String getNomService() { return nomService; }
+    public void setNomService(String nomService) { this.nomService = nomService; }
+
+    public double getMontantMensuel() { return montantMensuel; }
+    public void setMontantMensuel(double montantMensuel) { this.montantMensuel = montantMensuel; }
+
+    public LocalDate getDateDebut() { return dateDebut; }
+    public void setDateDebut(LocalDate dateDebut) { this.dateDebut = dateDebut; }
+
+    public LocalDate getDateFin() { return dateFin; }
+    public void setDateFin(LocalDate dateFin) { this.dateFin = dateFin; }
+
+    public StatusAbonnement getStatut() { return statut; }
+    public void setStatut(StatusAbonnement statut) { this.statut = statut; }
 
 
 }
