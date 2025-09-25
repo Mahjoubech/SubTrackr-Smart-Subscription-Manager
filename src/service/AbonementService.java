d15f9868fbf43b3ab8283e2143c47de811227504
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-25 10:29:29 +0100

    implimenter methodes ( add , update , delete , getAll ) : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-39

--- src/service/AbonementService.java
+++ src/service/AbonementService.java
@@ -1,4 +1,18 @@
 package service;
+import dao.AbonnementDao;
+import model.entity.Abonnement;
+import model.enums.StatusAbonnement;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 public class AbonementService {
+    private final AbonnementDao abonnementDao ;
+    public AbonementService(AbonnementDao abonnementDao) { this.abonnementDao = abonnementDao; }
+    public void addAbonnement(Abonnement abonnement) {abonnementDao.create(abonnement);}
+    public void updteAbonnement(Abonnement abonnement) {abonnementDao.update(abonnement);}
+    public void deleteAbonnement(Abonnement abonnement) {abonnementDao.delete(abonnement.getId());}
+    public Optional<Abonnement> getById(String id){return abonnementDao.findById(id);}
+    public List<Abonnement> getAll(){return abonnementDao.findAll();}
 }
