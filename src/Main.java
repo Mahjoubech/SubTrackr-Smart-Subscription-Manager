8a5360b7d406193d2513deef2dc4b46ca8f327a9
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-09-27 23:38:54 +0100

    Add Main : https://charkaouielmahjoub50-1758013914627.atlassian.net/browse/SDSPPA-54

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import dao.AbonnementDao;
-import model.entity.AbonnementAvecEngagement;
-import util.DbConn;
+import dao.PaiementDao;
+import service.AbonementService;
+import service.PaimentService;
+import service.RapportService;
+import ui.AbonnementMenu;
+import ui.PaiementMenu;
+import ui.RapportMenu;
+
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            DbConn db = DbConn.getInstance();
-            if (db.getConn() != null && !db.getConn().isClosed()) {
-                System.out.println("Connexion réussie !");
-            } else {
-                System.out.println("Échec de la connexion.");
-            }
-        } catch (Exception e) {
-            System.out.println("Erreur : " + e.getMessage());
-        }
         AbonnementDao dao = new AbonnementDao();
-        AbonnementAvecEngagement ab = new AbonnementAvecEngagement();
-        ab.setNom("Test Abonnement");
-        dao.create(ab);
-        // Affiche la liste pour vérifier l’ajout
-        System.out.println("Liste des abonnements : " + dao.getAbonnements());
+        AbonementService service = new AbonementService(dao);
+        PaiementDao pdao = new PaiementDao();
+        PaimentService  pservice = new PaimentService(pdao);
+        AbonnementMenu.initService(service);
+        PaiementMenu.initServices(pservice , service);
+        // You probably did this already
+        AbonnementMenu.initPaiementService(pservice);
+        RapportService rapportService = new RapportService(pdao);
+        RapportMenu.initServices(rapportService, service);
+        ui.Menu.afiche();
     }
-    }
-
+}
